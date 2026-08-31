6dd40ce0e5c66723ea6c55f862888c1190aa350f
gourav <gourav200611@gmail.com>
2026-08-31 20:15:06 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (83.24%) - LeetSync

---
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.c
@@ -0,0 +1,15 @@
+int maxProduct(int n) {
+    int digit1 = 0 , digit2 = 0 ;
+    while(n != 0 ){
+        int digit = n % 10 ;
+        if(digit > digit1){
+            digit2 = digit1;
+            digit1 = digit;
+        }
+        else if(digit > digit2){
+            digit2 = digit; 
+        }
+        n = n/ 10 ;
+    }
+    return digit1*digit2;
+}
