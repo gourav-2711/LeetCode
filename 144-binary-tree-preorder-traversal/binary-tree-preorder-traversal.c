33b6f64ca27865ce9c9d93d2d894ce27495dee9f
gourav <gourav200611@gmail.com>
2026-09-14 10:26:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (43.07%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.c
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.c
@@ -9,19 +9,18 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-
-void traversal(struct TreeNode *root,int *arr , int *index){
-    if(root == NULL)
-        return; 
-    arr[*index] = root->val;
-    (*index)++;
+void traversal(struct TreeNode *root , int arr[] , int *index){
+    if(root == NULL){
+        return ;
+    }
+    arr[(*index)++] = root->val;
     traversal(root->left , arr , index);
     traversal(root->right , arr , index);
 }
 int* preorderTraversal(struct TreeNode* root, int* returnSize) {
     int *arr = (int *)malloc(100 * sizeof(int));
-    int index = 0 ; 
-    traversal(root , arr , &index );
-    *returnSize = index;
-    return arr; 
+    int index = 0 ;
+    traversal(root , arr , &index);
+    *returnSize = index ;
+    return arr;
 }
